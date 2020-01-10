777ca3620ff3eeefb25787e0574f2e1e7fae662e
Justin Branch <justinms@outlook.com>
2020-01-10 19:35:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
     
     @@genres.each do |genre|
       if hash.has_key?(genre)
-        hash[:"#{genre}"] = value + 1 
+        hash[genre] += 1 
       else
         hash[genre] = 1 
       end 
